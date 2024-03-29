bfadae1ec138e1015d4609f99d3ccd6274949049
Moksh Shah <165096830+Newsrista@users.noreply.github.com>
2024-03-28 17:18:44 -0700

    Create search.js

---
+++ search.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const searchInput = document.getElementById('searchInput');
+  const postsDiv = document.querySelector('.posts');
+
+  searchInput.addEventListener('input', function(e) {
+    const searchTerm = e.target.value.toLowerCase();
+
+    fetch('/search_data.json')
+      .then(response => response.json())
+      .then(posts => {
+        postsDiv.innerHTML = ''; // Clear current posts
+        posts.filter(post => post.title.toLowerCase().includes(searchTerm) || post.excerpt.toLowerCase().includes(searchTerm))
+             .forEach(post => {
+               const postElement = `<article class="post">
+                 <h1><a href="${post.url}">${post.title}</a></h1>
+                 <div class="entry">${post.excerpt}</div>
+                 <a href="${post.url}" class="read-more">Read More</a>
+               </article>`;
+               postsDiv.insertAdjacentHTML('beforeend', postElement);
+             });
+      });
+  });
+});
